140e6c756835075e9ead8825de9bdf2021d19cf6
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-20 11:30:21 +0100

    change name of var X

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,23 +12,22 @@ const btn = document.querySelector(".btn");
 // bonus 1 fatto, solo penso proprio potrei scriverlo meglio, aspetto la correzione di Fabio Lunedì mattina 
 const select = document.querySelector("select");
 // ora dovrei associare a ogni option del select un valore diverso della X
-// ma come si fa?
 // potrei crearmi una funzione che tramite l'opzione del select mi cambia il valore della X
+let cellNumber = 10;
+
 select.addEventListener("change", function defineX () {
 
   if (select.value == "1") {
-    let X = 10;
-    console.log(X);
-    return X;
+    cellNumber = 10;
+    console.log(cellNumber);
   } else if (select.value == "2") {
-    let X = 9;
-    console.log(X);
-    return X;
+    cellNumber = 9;
+    console.log(cellNumber);
   } else {
-    let X = 7;
-    console.log(X);
-    return X;
+    cellNumber = 7;
+    console.log(cellNumber);
   }
+  return cellNumber;
 });
 
 /* questo non mi serve più, in teoria ahah
@@ -41,28 +40,28 @@ let X = Y;
 btn.addEventListener("click", function () {
   container.innerHTML = ""; //per svuotare il container
   // definisco la X anche dentro la funzione che genera la griglia
-  let X = 0;
+  let cellNumber = 0;
   if (select.value == "1") {
-    X = 10;
+    cellNumber = 10;
   } else if (select.value == "2") {
-    X = 9;
+    cellNumber = 9;
   } else {
-    X = 7;
+    cellNumber = 7;
   }
-  const grid = createGrid(X); //questa sarà la mia funzione che crea la griglia
+  const grid = createGrid(cellNumber); //questa sarà la mia funzione che crea la griglia
   container.appendChild(grid); //per mettere la griglia dentro il container
 });
 
 // ora mi serve la funzione createGrid, questa funzione deve generare nel container una griglia X^2 >(devo poter scegliere il valore della X)
 
-function createGrid(X) {
+function createGrid(cellNumber) {
   const grid = document.createElement('div');
   let number = 1;
   // Mi creo tante righe quant'è il valore di X e dentro mi creo altrettante colonne sempre seguendo il valore della X 
-  for (let i = 0; i < X; i++) {
+  for (let i = 0; i < cellNumber; i++) {
     const row = document.createElement('div');
     row.classList.add("my_row")
-    for (let j = 0; j < X; j++) {
+    for (let j = 0; j < cellNumber; j++) {
       const col = document.createElement('div');
       col.classList.add("my_col")
       col.innerText = number++
